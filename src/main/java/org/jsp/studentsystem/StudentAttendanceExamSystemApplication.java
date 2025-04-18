e3ee1df05c692cc2fa995daeb672294382b1cf8e
Midhun J <midhunjohn06@gmail.com>
2025-04-19 02:38:35 +0530

    Initial commit - spring boot application loaded

---
+++ src/main/java/org/jsp/studentsystem/StudentAttendanceExamSystemApplication.java
@@ -0,0 +1,13 @@
+package org.jsp.studentsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentAttendanceExamSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentAttendanceExamSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/jsp/studentsystem/StudentAttendanceExamSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jsp.studentsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentAttendanceExamSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
